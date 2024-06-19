70354824252b44ba08e13ff33ba4d7fb2c0d98b6
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-18 23:54:53 -0300

    feat(file): added .prettierrc.cjs file

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  arrowParens: 'avoid',
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  // Por algún motivo, al setear printWidth: 100, en realidad te permite 101 lineas de largo
+  printWidth: 99,
+}
