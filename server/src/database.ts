9aab23d9b4354168fa4fcece5521b457c8a85d22
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-23 23:41:43 -0300

    feat(database): connected to mongodb database

---
+++ server/src/database.ts
@@ -0,0 +1,28 @@
+import { configDotenv } from 'dotenv'
+import mongoose from 'mongoose'
+
+configDotenv()
+
+const { MONGO_URI } = process.env
+
+if (!MONGO_URI) {
+  throw new Error('Por favor define la variable MONGO_URI en el archivo .env')
+}
+
+const clientOptions = {
+  serverApi: { version: '1', strict: true, deprecationErrors: true },
+}
+
+export const connectDB = async () => {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(MONGO_URI, clientOptions)
+    await mongoose.connection.db.admin().command({ ping: 1 })
+    console.log('MongoDB conectado')
+  } catch (error) {
+    console.error(error)
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect()
+  }
+}
