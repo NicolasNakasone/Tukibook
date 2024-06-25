2e304637cbc38bc5b4aa92af9707158058d38f85
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-25 01:53:03 -0300

    fix(timestamp): added timestamp fields to post schema

--- server/src/models/Post.ts
+++ server/src/models/Post.ts
@@ -7,11 +7,14 @@ export interface IPost extends Document {
   comments: number
 }
 
-const PostSchema: Schema = new Schema({
-  username: { type: String, required: true },
-  content: { type: String, required: true },
-  likes: { type: Number, default: 0 },
-  comments: { type: Number, default: 0 },
-})
+const PostSchema: Schema = new Schema(
+  {
+    username: { type: String, required: true },
+    content: { type: String, required: true },
+    likes: { type: Number, default: 0 },
+    comments: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+)
 
 export const Post = mongoose.model<IPost>('Post', PostSchema)
