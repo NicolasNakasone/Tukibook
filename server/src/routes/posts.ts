7e7849a5ac663dcf79f6ae9b2d58e7eaf7c29c7a
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-25 01:19:46 -0300

    feat(handlers): created POST and GET handlers on postsRouter

--- server/src/routes/posts.ts
+++ server/src/routes/posts.ts
@@ -1,10 +1,32 @@
 import express from 'express'
+import { Post } from 'src/models/Post'
 
 export const postsRouter = express.Router()
 
-postsRouter.get('/', (req, res, next) => {
+postsRouter.get('/', async (req, res, next) => {
   try {
-    res.send('posts')
+    const posts = await Post.find({})
+    const mappedPosts = posts.map(({ username, content, likes, comments }) => ({
+      username,
+      content,
+      likes,
+      comments,
+    }))
+
+    res.send(mappedPosts)
+  } catch (error) {
+    next(error)
+  }
+})
+
+postsRouter.post('/', async (req, res, next) => {
+  try {
+    const { username, content, likes, comments } = req.body
+
+    const newPost = new Post({ username, content, likes, comments })
+    const savedPost = await newPost.save()
+
+    res.status(201).send(savedPost)
   } catch (error) {
     next(error)
   }
