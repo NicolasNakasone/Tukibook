6dcf308349743a640c81819459b6afc368e3be5c
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-26 21:25:16 -0300

    feat(router): created commentsRouter

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import { routes } from 'src/constants/routes'
+import { commentsRouter } from 'src/routes/comments'
 import { postsRouter } from 'src/routes/posts'
 
 export const router = express.Router()
 
 router.use(routes.posts, postsRouter)
+router.use(routes.comments, commentsRouter)
 
 router.get(routes.home, (req, res, next) => {
   try {
