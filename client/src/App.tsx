c9eaee871595d227f29c827b1be4bc65e527a591
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-27 19:50:20 -0300

    refactor(html): replaced div with header on App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,10 @@ import tukibookLogo from 'public/tuki.webp'
 export const App = () => {
   return (
     <>
-      <div style={{ display: 'flex' }}>
-        <h1>Tukibook! </h1>
+      <header style={{ display: 'flex' }}>
+        <h1>Tukibook!</h1>
         <img src={tukibookLogo} alt="Tukibook Logo" style={{ width: '2rem', height: '2rem' }} />
-      </div>
+      </header>
       <p>Una red social, tuki 👍</p>
     </>
   )
