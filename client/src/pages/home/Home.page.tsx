22fd5e05e41d2ed0b464a44381ca5d7727e8f629
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-28 23:40:00 -0300

    feat(page): created HomePage page

---
+++ client/src/pages/home/Home.page.tsx
@@ -0,0 +1,10 @@
+import { Header } from 'src/components/common/Header'
+
+export const HomePage = (): JSX.Element => {
+  return (
+    <>
+      <Header />
+      <p>Una red social, tuki 👍</p>
+    </>
+  )
+}
