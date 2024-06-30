9814b525c3ea7d395dda344382262f62903f995d
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-30 05:23:28 -0300

    refactor(components): replaced inline elements in App.tsx with common components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,24 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Outlet } from 'react-router-dom'
+import { Footer } from 'src/components/common/Footer'
+import { Header } from 'src/components/common/Header'
 import { routes } from 'src/constants/routes'
 import { HomePage } from 'src/pages'
 
 export const App = () => {
   return (
     <Routes>
-      <Route path={routes.home} element={<HomePage />} />
+      <Route
+        path={routes.home}
+        element={
+          <>
+            <Header />
+            <Outlet />
+            <Footer />
+          </>
+        }
+      >
+        <Route path={routes.home} element={<HomePage />} />
+      </Route>
     </Routes>
   )
 }
