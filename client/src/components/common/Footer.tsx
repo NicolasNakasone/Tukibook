9c5edb42db823692a75a6ea116bd734d014681e3
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-30 04:50:17 -0300

    feat(components): created SeeMoreButton and Footer components

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,20 @@
+export const Footer = (): JSX.Element => {
+  return (
+    <footer
+      style={{
+        width: '100%',
+        padding: '4rem 0 0',
+        margin: '0 0 1rem',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '1rem',
+        borderTop: '1px solid CanvasText',
+      }}
+    >
+      <p>Hecho con ❤️</p>
+      {/* <p>RRSS</p> */}
+    </footer>
+  )
+}
---
+++ client/src/components/common/SeeMoreButton.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+export const SeeMoreButton = ({ content }: { content: string }): JSX.Element | string => {
+  const [showMore, setShowMore] = useState(false)
+
+  if (showMore) return content
+
+  return (
+    <span style={{ cursor: 'pointer' }} onClick={() => setShowMore(true)}>
+      {`${content.slice(0, 90)}... `}
+      <span style={{ fontWeight: 600 }}>Ver más</span>
+    </span>
+  )
+}
