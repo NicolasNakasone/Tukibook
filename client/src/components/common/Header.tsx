801dfbd82401c2837db36b607f7e1a6468e46b03
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-06-30 05:21:18 -0300

    refactor(html): modified html code in Header.tsx

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -2,9 +2,28 @@ import tukibookLogo from 'public/tuki.webp'
 
 export const Header = (): JSX.Element => {
   return (
-    <header style={{ display: 'flex' }}>
-      <h1>Tukibook!</h1>
-      <img src={tukibookLogo} alt="Tukibook Logo" style={{ width: '2rem', height: '2rem' }} />
+    <header
+      style={{
+        width: '100%',
+        // margin: '0 0 2rem',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        borderBottom: '1px solid CanvasText',
+      }}
+    >
+      <div
+        role="heading"
+        style={{ margin: '1rem 0 0', display: 'flex', alignItems: 'center', gap: '1rem' }}
+      >
+        <h1>Tukibook!</h1>
+        <img
+          src={tukibookLogo}
+          alt="Tukibook Logo"
+          style={{ width: '2rem', height: '2rem', borderRadius: '0.25rem' }}
+        />
+      </div>
+      <p style={{ margin: '0 0 2rem' }}>Una red social, tuki 👍</p>
     </header>
   )
 }
