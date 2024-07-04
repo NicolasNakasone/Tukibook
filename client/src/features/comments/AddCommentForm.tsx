77a60e8c11112a6d6d4541eee0c989db507fc4c8
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-07-04 00:07:41 -0300

    refactor(dispatch): replaced usePosts with dispatch in AddCommentForm

--- client/src/features/comments/AddCommentForm.tsx
+++ client/src/features/comments/AddCommentForm.tsx
@@ -1,25 +1,27 @@
 import { FormEvent } from 'react'
 
-import { usePosts } from 'src/hooks/usePosts.hook'
+import { useDispatch } from 'react-redux'
 import styles from 'src/pages/home/Home.module.css'
+import { commentPost } from 'src/states/slices/postsSlice'
+import { AppDispatch } from 'src/states/store'
 import { Post } from 'src/types'
 
 export const AddCommentForm = ({ post }: { post: Post }): JSX.Element => {
-  const { commentPost } = usePosts()
+  const dispatch = useDispatch<AppDispatch>()
 
-  const handleCommentPost = async (event: FormEvent<HTMLFormElement>, postId: string) => {
+  const handleCommentPost = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
 
     const target = event.target as HTMLFormElement
-    const content = target[0] as HTMLInputElement
+    const content = (target[0] as HTMLInputElement).value
 
-    await commentPost({ postId, username: 'otro user', content: content.value })
+    await dispatch(commentPost({ postId: post.id, username: 'otro user', content }))
 
-    content.value = ''
+    target.reset()
   }
 
   return (
-    <form style={{ width: '100%' }} onSubmit={e => handleCommentPost(e, post.id)}>
+    <form style={{ width: '100%' }} onSubmit={handleCommentPost}>
       <input type="text" placeholder="Comentar..." className={styles.commentInput} />
     </form>
   )
