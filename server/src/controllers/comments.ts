553648489e1d26ae2ace2a16dd9440274cfefe98
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-07-05 20:49:33 -0300

    feat(controller): created comments controllers

---
+++ server/src/controllers/comments.ts
@@ -0,0 +1,29 @@
+import { RequestHandler } from 'express'
+import { Comment } from 'src/models/Comment'
+import { Post } from 'src/models/Post'
+import { validateRequiredFields } from 'src/utils'
+
+export const addCommentToPost: RequestHandler = async (req, res, next) => {
+  try {
+    const { postId, username, content } = req.body
+
+    // Primero se valida que todos los datos requeridos existan
+    if (!validateRequiredFields(postId, username, content))
+      return res.status(400).send({ message: 'Faltan datos para crear el comentario' })
+
+    // Luego se valida que el post exista antes de crear el comentario
+    const foundPost = await Post.findById(postId)
+    if (!foundPost) return res.status(404).send({ message: 'Post no encontrado' })
+
+    const newComment = new Comment({ postId, username, content })
+    const savedComment = await newComment.save()
+
+    // Solo se guarda el id del comentario, por mas que savedComment sea un IComment
+    foundPost.comments.push(savedComment)
+    await foundPost.save()
+
+    res.status(201).send(savedComment)
+  } catch (error) {
+    next(error)
+  }
+}
