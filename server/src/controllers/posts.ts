3a7bc70874670b535bd6c2449dd019b450a80526
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-07-05 20:45:33 -0300

    feat(folder): created controllers folder

---
+++ server/src/controllers/posts.ts
@@ -0,0 +1,68 @@
+import { RequestHandler } from 'express'
+import mongoose from 'mongoose'
+import { Post } from 'src/models/Post'
+import { validateRequiredFields } from 'src/utils'
+
+export const getPosts: RequestHandler = async (req, res, next) => {
+  try {
+    // Con populate se puede hacer un get a los comentarios, con los ids guardados en comments
+    const posts = await Post.find({}).populate('comments').sort({ createdAt: -1 })
+
+    res.send(posts)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const getPostById: RequestHandler = async (req, res, next) => {
+  const { id: postId } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(postId)) {
+    return res.status(400).send({ message: 'Id del post inválido' })
+  }
+
+  try {
+    const foundPost = await Post.findById(postId).populate('comments')
+    if (!foundPost) return res.status(404).send({ message: 'Post no encontrado' })
+
+    res.send(foundPost)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const addPost: RequestHandler = async (req, res, next) => {
+  try {
+    // No tiene sentido que un post antes de crearse ya tenga likes o comentarios
+    const { username, content } = req.body
+
+    if (!validateRequiredFields(username, content))
+      return res.status(400).send({ message: 'Faltan datos para crear el post' })
+
+    const newPost = new Post({ username, content })
+    const savedPost = await newPost.save()
+
+    res.status(201).send(savedPost)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const deletePost: RequestHandler = async (req, res, next) => {
+  const { id: postId } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(postId)) {
+    return res.status(400).send({ message: 'Id del post inválido' })
+  }
+
+  try {
+    const deletedPost = await Post.findByIdAndDelete(postId)
+    if (!deletedPost) {
+      return res.status(404).send({ message: 'Post no encontrado' })
+    }
+
+    res.send(deletedPost)
+  } catch (error) {
+    next(error)
+  }
+}
