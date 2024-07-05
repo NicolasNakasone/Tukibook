140fc123298e5c9dbab0be93e15ab303759c3ce1
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-07-05 20:53:29 -0300

    feat(controller): created likes controller

---
+++ server/src/controllers/likes.ts
@@ -0,0 +1,23 @@
+import { RequestHandler } from 'express'
+import mongoose from 'mongoose'
+import { Post } from 'src/models/Post'
+
+export const addLikeToPost: RequestHandler = async (req, res, next) => {
+  const { id: postId } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(postId)) {
+    return res.status(400).send({ message: 'Id del post inválido' })
+  }
+
+  try {
+    const foundPost = await Post.findById(postId)
+    if (!foundPost) return res.status(404).send({ message: 'Post no encontrado' })
+
+    foundPost.likes += 1
+    const updatedPost = await foundPost.save()
+
+    res.send(updatedPost)
+  } catch (error) {
+    next(error)
+  }
+}
