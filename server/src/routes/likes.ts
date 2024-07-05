ff6a3bec80c95c0a8dbe20c99e940560324e88e9
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-07-05 20:53:51 -0300

    refactor(controller): replaced likes handler with likes controller

--- server/src/routes/likes.ts
+++ server/src/routes/likes.ts
@@ -1,25 +1,6 @@
 import express from 'express'
-import mongoose from 'mongoose'
-import { Post } from 'src/models/Post'
+import { addLikeToPost } from 'src/controllers/likes'
 
 export const likesRouter = express.Router()
 
-likesRouter.patch('/:id', async (req, res, next) => {
-  const { id: postId } = req.params
-
-  if (!mongoose.Types.ObjectId.isValid(postId)) {
-    return res.status(400).send({ message: 'Id del post inválido' })
-  }
-
-  try {
-    const foundPost = await Post.findById(postId)
-    if (!foundPost) return res.status(404).send({ message: 'Post no encontrado' })
-
-    foundPost.likes += 1
-    const updatedPost = await foundPost.save()
-
-    res.send(updatedPost)
-  } catch (error) {
-    next(error)
-  }
-})
+likesRouter.patch('/:id', addLikeToPost)
