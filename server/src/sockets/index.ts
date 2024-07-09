fd6af44bca37c4f573b7fd9159327d53c1394915
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-07-08 22:56:10 -0300

    fix(cors): added cors handle to new Server instance from socket io

--- server/src/sockets/index.ts
+++ server/src/sockets/index.ts
@@ -1,13 +1,20 @@
+/* eslint-disable no-console */
 import { Server as HttpServer } from 'http'
 
 import { Server } from 'socket.io'
 import { handleSocketEvents } from 'src/sockets/events'
 
+const { CLIENT_URL } = process.env
+
 export const initializeSocket = (server: HttpServer): void => {
-  const io = new Server(server)
+  const io = new Server(server, {
+    cors: {
+      origin: CLIENT_URL, // Cambia esto a la IP y puerto de tu frontend
+      methods: ['GET', 'POST'],
+    },
+  })
   console.log('Socket iniciado en el servidor!')
   io.on('connection', socket => {
-    console.log('a user connected', socket.id)
     handleSocketEvents(io, socket)
 
     socket.on('disconnect', () => {
