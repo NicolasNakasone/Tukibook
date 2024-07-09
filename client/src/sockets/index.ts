7d02e13512f14752ba87bbd5452c7e2a48dd6273
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-07-09 20:19:07 -0300

    feat(event): created emitDeletedPost event on client side

--- client/src/sockets/index.ts
+++ client/src/sockets/index.ts
@@ -1,10 +1,15 @@
 import { io } from 'socket.io-client'
 import { Post } from 'src/types'
+import { SocketEvents } from 'src/types/socket'
 
 const { VITE_API_URL } = import.meta.env
 
 export const socket = io(VITE_API_URL)
 
 export const emitNewPost = (newPost: Post) => {
-  socket.emit('newPost', newPost)
+  socket.emit(SocketEvents.NEW_POST, newPost)
+}
+
+export const emitDeletedPost = (deletedPost: Post) => {
+  socket.emit(SocketEvents.DELETED_POST, deletedPost)
 }
