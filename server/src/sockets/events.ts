90be6d911a96cadb0a46b6b69b35ed5c0283a7ac
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-07-10 17:07:00 -0300

    feat(readme): added readme to app

--- server/src/sockets/events.ts
+++ server/src/sockets/events.ts
@@ -4,4 +4,8 @@ export const handleSocketEvents = (io: Server, socket: Socket): void => {
   socket.on('newPost', (post /* : Post // TODO: Crear tipo Post */) => {
     io.emit('newPost', post) // Emite el post a todos los clientes conectados
   })
+
+  socket.on('deletedPost', (post /* : Post // TODO: Crear tipo Post */) => {
+    io.emit('deletedPost', post) // Emite el post que se borro a todos los clientes conectados
+  })
 }
--- server/src/sockets/index.ts
+++ server/src/sockets/index.ts
@@ -7,18 +7,14 @@ import { handleSocketEvents } from 'src/sockets/events'
 const { CLIENT_URL } = process.env
 
 export const initializeSocket = (server: HttpServer): void => {
-  const io = new Server(server, {
-    cors: {
-      origin: CLIENT_URL, // Cambia esto a la IP y puerto de tu frontend
-      methods: ['GET', 'POST'],
-    },
-  })
+  const io = new Server(server, { cors: { origin: CLIENT_URL, methods: ['GET', 'POST'] } })
   console.log('Socket iniciado en el servidor!')
   io.on('connection', socket => {
+    console.log('Usuario conectado: ', socket.id)
     handleSocketEvents(io, socket)
 
     socket.on('disconnect', () => {
-      console.log('user disconnected')
+      console.log('Usuario desconectado')
     })
   })
 }
