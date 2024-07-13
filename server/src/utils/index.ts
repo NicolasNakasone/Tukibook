3b999d404969c8e3140189599d9be8447979c45a
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-07-13 00:19:49 -0300

    feat(function): created isValidObjectId function on utils folder

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,3 +1,4 @@
+import { isValidObjectId } from 'src/utils/isValidObjectId'
 import { validateRequiredFields } from 'src/utils/validateRequiredFields'
 
-export { validateRequiredFields }
+export { isValidObjectId, validateRequiredFields }
