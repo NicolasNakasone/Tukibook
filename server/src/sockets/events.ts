693d1bd5fb88cf27ac0bdca354486290a150cf48
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-07-12 23:11:54 -0300

    feat(event): added commentPost socket event on server side

--- server/src/sockets/events.ts
+++ server/src/sockets/events.ts
@@ -8,4 +8,8 @@ export const handleSocketEvents = (io: Server, socket: Socket): void => {
   socket.on('deletePost', (post /* : Post // TODO: Crear tipo Post */) => {
     io.emit('deletePost', post) // Emite el post que se borro a todos los clientes conectados
   })
+
+  socket.on('commentPost', (comment /* : Comment // TODO: Crear tipo Comment */) => {
+    io.emit('commentPost', comment) // Emite el comentario que se creo a todos los clientes conectados
+  })
 }
