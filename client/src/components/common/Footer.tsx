a5eae411dc865a2a698c7060066563427d029446
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-07-24 23:20:59 -0300

    refactor(module): moved all inline styles to Footer.module

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -1,18 +1,8 @@
+import 'src/components/common/Footer.module.css'
+
 export const Footer = (): JSX.Element => {
   return (
-    <footer
-      style={{
-        width: '100%',
-        padding: '4rem 0 0',
-        margin: '0 0 1rem',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: '1rem',
-        borderTop: '1px solid CanvasText',
-      }}
-    >
+    <footer>
       <p>Hecho con ❤️</p>
       {/* <p>RRSS</p> */}
     </footer>
