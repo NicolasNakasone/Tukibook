c528115e76b46c13876f0ed8a3dc499a23d057e9
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-08-05 23:59:13 -0300

    feat(file): created file for common post related types

---
+++ packages/types/posts.ts
@@ -0,0 +1,19 @@
+export interface PostInput {
+  username: string
+  content: string
+}
+
+export interface Post extends PostInput {
+  id: string
+  likes: number
+  comments: Comment[]
+  createdAt: Date
+  updatedAt: Date
+}
+
+export type PostList = Post[]
+
+export interface GetPostsResponse {
+  posts: PostList
+  totalItems: number
+}
