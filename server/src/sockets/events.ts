dbf19a2d90f258dd57d7d58ddebaed4941dc9c26
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-10-16 23:41:59 -0300

    feat(package): installed tukibook-helper package on server

--- server/src/sockets/events.ts
+++ server/src/sockets/events.ts
@@ -1,19 +1,20 @@
 import { Server, Socket } from 'socket.io'
+import { SocketEvents } from 'tukibook-helper'
 
 export const handleSocketEvents = (io: Server, socket: Socket): void => {
-  socket.on('newPost', (post /* : Post // TODO: Crear tipo Post */) => {
-    io.emit('newPost', post) // Emite el post a todos los clientes conectados
+  socket.on(SocketEvents.NEW_POST, (post /* : Post // TODO: Crear tipo Post */) => {
+    io.emit(SocketEvents.NEW_POST, post) // Emite el post a todos los clientes conectados
   })
 
-  socket.on('deletePost', (post /* : Post // TODO: Crear tipo Post */) => {
-    io.emit('deletePost', post) // Emite el post que se borro a todos los clientes conectados
+  socket.on(SocketEvents.DELETE_POST, (post /* : Post // TODO: Crear tipo Post */) => {
+    io.emit(SocketEvents.DELETE_POST, post) // Emite el post que se borro a todos los clientes conectados
   })
 
-  socket.on('commentPost', (comment /* : Comment // TODO: Crear tipo Comment */) => {
-    io.emit('commentPost', comment) // Emite el comentario que se creo a todos los clientes conectados
+  socket.on(SocketEvents.COMMENT_POST, (comment /* : Comment // TODO: Crear tipo Comment */) => {
+    io.emit(SocketEvents.COMMENT_POST, comment) // Emite el comentario que se creo a todos los clientes conectados
   })
 
-  socket.on('likePost', post => {
-    io.emit('likePost', post) // Emite el post que se 'likeo' a todos los clientes conectados
+  socket.on(SocketEvents.LIKE_POST, post => {
+    io.emit(SocketEvents.LIKE_POST, post) // Emite el post que se 'likeo' a todos los clientes conectados
   })
 }
