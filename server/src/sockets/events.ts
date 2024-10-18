33bd74dafa3da84d3f49a4e67492dd030a01b686
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2024-10-17 21:30:49 -0300

    feat(type): typed params from handleSocketEvents events

--- server/src/sockets/events.ts
+++ server/src/sockets/events.ts
@@ -1,20 +1,20 @@
 import { Server, Socket } from 'socket.io'
-import { SocketEvents } from 'tukibook-helper'
+import { Comment, Post, SocketEvents } from 'tukibook-helper'
 
 export const handleSocketEvents = (io: Server, socket: Socket): void => {
-  socket.on(SocketEvents.NEW_POST, (post /* : Post // TODO: Crear tipo Post */) => {
+  socket.on(SocketEvents.NEW_POST, (post: Post) => {
     io.emit(SocketEvents.NEW_POST, post) // Emite el post a todos los clientes conectados
   })
 
-  socket.on(SocketEvents.DELETE_POST, (post /* : Post // TODO: Crear tipo Post */) => {
+  socket.on(SocketEvents.DELETE_POST, (post: Post) => {
     io.emit(SocketEvents.DELETE_POST, post) // Emite el post que se borro a todos los clientes conectados
   })
 
-  socket.on(SocketEvents.COMMENT_POST, (comment /* : Comment // TODO: Crear tipo Comment */) => {
+  socket.on(SocketEvents.COMMENT_POST, (comment: Comment) => {
     io.emit(SocketEvents.COMMENT_POST, comment) // Emite el comentario que se creo a todos los clientes conectados
   })
 
-  socket.on(SocketEvents.LIKE_POST, post => {
+  socket.on(SocketEvents.LIKE_POST, (post: Post) => {
     io.emit(SocketEvents.LIKE_POST, post) // Emite el post que se 'likeo' a todos los clientes conectados
   })
 }
