187df9311f4fb8c5b5c7bedbd3a8c23b420cc36e
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2025-04-24 23:13:07 -0300

    refactor(broadcast): replaced io with broadcast on socket events from server

--- server/src/sockets/events.ts
+++ server/src/sockets/events.ts
@@ -3,30 +3,30 @@ import { Post, SocketEvents } from 'tukibook-helper'
 
 export const handleSocketEvents = (io: Server, socket: Socket): void => {
   socket.on(SocketEvents.NEW_POST, (post: Post) => {
-    io.emit(SocketEvents.NEW_POST, post) // Emite el post a todos los clientes conectados
+    socket.broadcast.emit(SocketEvents.NEW_POST, post)
   })
 
   socket.on(SocketEvents.DELETE_POST, (post: Post) => {
-    io.emit(SocketEvents.DELETE_POST, post) // Emite el post que se borro a todos los clientes conectados
+    socket.broadcast.emit(SocketEvents.DELETE_POST, post)
   })
 
   socket.on(SocketEvents.LIKE_POST, (post: Post) => {
-    io.emit(SocketEvents.LIKE_POST, post) // Emite el post que se 'likeo' a todos los clientes conectados
+    socket.broadcast.emit(SocketEvents.LIKE_POST, post)
   })
 
   socket.on(SocketEvents.EDIT_POST, (post: Post) => {
-    io.emit(SocketEvents.EDIT_POST, post) // Emite el post que se edito a todos los clientes conectados
+    socket.broadcast.emit(SocketEvents.EDIT_POST, post)
   })
 
   socket.on(SocketEvents.COMMENT_POST, (post: Post) => {
-    io.emit(SocketEvents.COMMENT_POST, post) // Emite el comentario que se creo a todos los clientes conectados
+    socket.broadcast.emit(SocketEvents.COMMENT_POST, post)
   })
 
   socket.on(SocketEvents.EDIT_COMMENT, (post: Post) => {
-    io.emit(SocketEvents.EDIT_COMMENT, post) // Emite el comentario que se edito a todos los clientes conectados
+    socket.broadcast.emit(SocketEvents.EDIT_COMMENT, post)
   })
 
   socket.on(SocketEvents.DELETE_COMMENT, (post: Post) => {
-    io.emit(SocketEvents.DELETE_COMMENT, post) // Emite el comentario que se borro a todos los clientes conectados
+    socket.broadcast.emit(SocketEvents.DELETE_COMMENT, post)
   })
 }
