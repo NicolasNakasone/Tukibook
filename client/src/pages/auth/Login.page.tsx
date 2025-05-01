46eb4239d5a64b1804e30a53dd50ef08d89bd71e
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2025-04-30 23:57:01 -0300

    feat(login): finished login user on client

--- client/src/pages/auth/Login.page.tsx
+++ client/src/pages/auth/Login.page.tsx
@@ -1,7 +1,47 @@
-import { Link } from 'react-router-dom'
+import { FormEvent } from 'react'
+
+import { Link, useNavigate } from 'react-router-dom'
 import { Button } from 'src/components/common/Button'
+import { handleFetch } from 'src/constants/api'
+import { routes } from 'src/constants/routes'
+
+const { VITE_API_URL } = import.meta.env
 
 export const LoginPage = (): JSX.Element => {
+  const navigate = useNavigate()
+
+  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const target = e.target as HTMLFormElement
+
+    const email = target[0] as HTMLInputElement
+    const password = target[1] as HTMLInputElement
+
+    const loggedUser = {
+      email: email.value,
+      password: password.value,
+    }
+
+    const loginUser = await handleFetch(`${VITE_API_URL}${routes.login}`, {
+      method: 'POST',
+      body: JSON.stringify(loggedUser),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+    })
+
+    const response = await loginUser?.json()
+
+    console.log({ response })
+    if (response.message) return // setError o algo asi
+
+    target.reset()
+    // Guardar token en localStorage
+    navigate(routes.home)
+  }
+
   return (
     <main
       style={{
@@ -12,7 +52,10 @@ export const LoginPage = (): JSX.Element => {
       }}
     >
       <h1 style={{ margin: '0 0 2rem' }}>Bienvenido a Tukibook 👍!</h1>
-      <form style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+      <form
+        style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}
+        onSubmit={handleLogin}
+      >
         <input name="email" type="email" placeholder="✉️ Ingresa tu correo" />
         <input name="password" type="password" placeholder="🤫 Ingresa tu contraseña" />
         <Button style={{ margin: '0 0 0 auto' }}>Iniciar sesión</Button>
