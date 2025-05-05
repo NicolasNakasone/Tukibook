03bba45d81ee25b30979cb2292fee4d770a3ea0f
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2025-05-04 21:52:57 -0300

    feat(type): added user properties to comment interface

--- tukibook-helper/types/comments.ts
+++ tukibook-helper/types/comments.ts
@@ -1,12 +1,14 @@
+import { UserPayload } from './users'
+
 export interface CommentInput {
   postId: string
-  username: string
   content: string
   parentCommentId: string | null
 }
 
 export interface Comment extends CommentInput {
   id: string
+  user: UserPayload
   createdAt: Date
   updatedAt: Date
 }
