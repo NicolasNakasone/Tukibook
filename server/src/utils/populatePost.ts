233c517469d2ea37e7501fc7f5a5a9cbfe0dfdfc
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2025-05-07 02:29:11 -0300

    feat(util): created populatePost util function on server

--- server/src/utils/populatePost.ts
+++ server/src/utils/populatePost.ts
@@ -1,7 +1,16 @@
-import mongoose from 'mongoose'
-import { Post } from 'src/models/Post'
+import mongoose, { HydratedDocument } from 'mongoose'
+import { IPost, Post } from 'src/models/Post'
 
-export const populateFullPost = (query: mongoose.Query<any, typeof Post>) => {
+export const populatePost = async (postDoc: HydratedDocument<IPost>) => {
+  await postDoc.populate({
+    path: 'comments',
+    populate: { path: 'user', select: 'username id' },
+  })
+  await postDoc.populate({ path: 'user', select: 'username id' })
+  return postDoc
+}
+
+export const populatePostQuery = (query: mongoose.Query<any, typeof Post>) => {
   return query
     .populate({ path: 'comments', populate: { path: 'user', select: 'username id' } })
     .populate({ path: 'user', select: 'username id' })
