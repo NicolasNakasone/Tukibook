085feea7794e3ee7c9275db7a28bc11d292474fb
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2025-05-12 01:36:21 -0300

    fix(headers): setted cors headers manually

--- server/src/server.ts
+++ server/src/server.ts
@@ -24,6 +24,14 @@ server.use(
   })
 )
 
+server.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', CLIENT_URL || '*')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+})
+
 server.set('port', API_PORT || 4000)
 
 server.use(logger('dev'))
