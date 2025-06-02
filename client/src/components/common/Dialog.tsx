2461efebf28564b71fff56da4f31ef5b47205b33
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2025-06-02 02:49:35 -0300

    refactor(module): moved Dialog styles to a css module

--- client/src/components/common/Dialog.tsx
+++ client/src/components/common/Dialog.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react'
 
 import { createPortal } from 'react-dom'
 import { Button } from 'src/components/common/Button'
+import styles from 'src/components/common/Dialog.module.css'
 import { useFocusTrap } from 'src/hooks/useFocusTrap.hook'
 
 const DIALOG_ROOT_ID = 'dialog-root'
@@ -44,33 +45,13 @@ export const Dialog = ({
   if (!isMounted || !open || !dialogRootRef.current) return null
 
   return createPortal(
-    <div
-      style={{
-        width: '100vw',
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        backgroundColor: '#00000080',
-        zIndex: 1000,
-      }}
-      {...(allowBackdropClose && { onClick: onClose })}
-    >
+    <div className={styles.dialogBackdrop} {...(allowBackdropClose && { onClick: onClose })}>
       <div
         ref={containerRef}
-        style={{
-          minWidth: '300px',
-          padding: '1.5rem',
-          backgroundColor: 'Canvas',
-          border: '1px solid CanvasText',
-          borderRadius: '0.25rem',
-        }}
+        className={styles.dialogContainer}
         onClick={e => e.stopPropagation()}
       >
-        <Button style={{ margin: '1rem 0 1rem auto', display: 'flex' }} onClick={onClose}>
+        <Button className={styles.dialogCloseButton} onClick={onClose}>
           {/* Cerrar  */}❌
         </Button>
         {children}
