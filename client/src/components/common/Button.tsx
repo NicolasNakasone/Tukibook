f8f8774a52aff46dd60cea6c43785dd22f7aeaa7
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2025-06-02 02:06:31 -0300

    feat(forwardRef): added forwardRef function to common Button component

--- client/src/components/common/Button.tsx
+++ client/src/components/common/Button.tsx
@@ -1,13 +1,17 @@
-import { ButtonHTMLAttributes } from 'react'
+import { forwardRef, ButtonHTMLAttributes } from 'react'
 
 import styles from 'src/components/common/Button.module.css'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
-export const Button = ({ children, className = '', ...rest }: ButtonProps): JSX.Element => {
-  return (
-    <button className={`${styles.button} ${className}`} {...rest}>
-      {children}
-    </button>
-  )
-}
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, className = '', ...rest }, ref) => {
+    return (
+      <button ref={ref} className={`${styles.button} ${className}`} {...rest}>
+        {children}
+      </button>
+    )
+  }
+)
+
+Button.displayName = 'Button'
