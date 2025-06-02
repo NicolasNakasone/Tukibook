9ac08b0fce8dc81ce0788f7807b3e969f71ee36f
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2025-06-02 02:24:33 -0300

    fix(styles): improved styles of PasswordInput

--- client/src/components/form/PasswordInput.tsx
+++ client/src/components/form/PasswordInput.tsx
@@ -1,6 +1,7 @@
-import { InputHTMLAttributes, useState } from 'react'
+import { InputHTMLAttributes, useEffect, useRef, useState } from 'react'
 
 import { Button } from 'src/components/common/Button'
+import styles from 'src/components/form/PasswordInput.module.css'
 
 interface PasswordInputProps {
   inputProps?: InputHTMLAttributes<HTMLInputElement>
@@ -10,21 +11,30 @@ export const PasswordInput = ({
   inputProps = { name: 'password', placeholder: '🤫 Ingresa tu contraseña' },
 }: PasswordInputProps): JSX.Element => {
   const [isPassword, setIsPassword] = useState(true)
+  const buttonRef = useRef<HTMLButtonElement | null>(null)
+  const [buttonWidth, setButtonWidth] = useState(0)
+
+  useEffect(() => {
+    if (buttonRef.current) {
+      setButtonWidth(buttonRef.current.offsetWidth)
+    }
+  }, [])
 
   const togglePassword = () => {
     setIsPassword(prevPassword => !prevPassword)
   }
 
   return (
-    <>
+    <div className={styles.passwordInputContainer}>
       <input
         name={inputProps.name}
         type={isPassword ? 'password' : 'text'}
         placeholder={inputProps.placeholder}
+        style={{ paddingRight: `${buttonWidth + 24}px` }}
       />
-      <Button type="button" onClick={togglePassword}>
+      <Button ref={buttonRef} type="button" onClick={togglePassword}>
         {isPassword ? `Mostrar 🧐` : `Ocultar 😴`}
       </Button>
-    </>
+    </div>
   )
 }
