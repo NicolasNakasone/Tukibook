c1422bb7403bf5e33ea1db44040bcc2cc272597f
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2025-06-04 00:37:28 -0300

    feat(value): added value link to variant prop from Button component

--- client/src/components/common/Button.tsx
+++ client/src/components/common/Button.tsx
@@ -1,28 +1,49 @@
-import { forwardRef, ButtonHTMLAttributes } from 'react'
+import { forwardRef, ButtonHTMLAttributes, Ref } from 'react'
 
+import { Link, LinkProps } from 'react-router-dom'
 import styles from 'src/components/common/Button.module.css'
 
-type ButtonVariant = 'normal' | 'outline'
+type ButtonVariant = 'normal' | 'outline' | 'link'
 type ButtonSize = 'sm' | 'md' | 'lg'
 type ButtonColor = 'primary' | 'success' | 'error' | 'warning' | 'info'
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: ButtonVariant
+interface ButtonCommonProps {
   size?: ButtonSize
   color?: ButtonColor
 }
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+interface ButtonAsButton extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: Exclude<ButtonVariant, 'link'>
+}
+
+interface ButtonAsLink extends LinkProps {
+  variant: Extract<ButtonVariant, 'link'>
+}
+
+type ButtonProps = (ButtonAsButton | ButtonAsLink) & ButtonCommonProps
+
+export const Button = forwardRef<HTMLButtonElement | HTMLAnchorElement, ButtonProps>(
   (
     { children, className = '', variant = 'outline', size = 'sm', color = 'primary', ...rest },
     ref
   ) => {
+    const classes = `${styles[variant]} ${styles[size]} ${styles[color]} ${className}`
+
+    if (variant === 'link') {
+      const linkProps = rest as LinkProps
+      return (
+        <Link ref={ref as Ref<HTMLAnchorElement>} className={classes} {...linkProps}>
+          {children}
+        </Link>
+      )
+    }
+
     return (
       <button
-        ref={ref}
+        ref={ref as Ref<HTMLButtonElement>}
         type="button"
-        className={`${styles[variant]} ${styles[size]} ${styles[color]} ${className}`}
-        {...rest}
+        className={classes}
+        {...(rest as ButtonHTMLAttributes<HTMLButtonElement>)}
       >
         {children}
       </button>
