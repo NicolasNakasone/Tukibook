f3e613a598010c1205c435813b5d764c0a3727bd
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2025-07-11 03:00:44 -0300

    feat(slice): started comments slice

---
+++ client/src/states/slices/commentsSlice.ts
@@ -0,0 +1,69 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { handleFetch } from 'src/constants/api'
+import { Comment } from 'tukibook-helper'
+
+export interface CommentsState {
+  commentsByPostId: {
+    [postId: string]: {
+      error: string | null
+      comments: Comment[]
+      totalItems: number
+      isLoading: boolean
+      hasMore: boolean
+      page: number
+    }
+  }
+}
+
+const initialState: CommentsState = {
+  commentsByPostId: {},
+}
+
+type CommentPayload = Awaited<ReturnType<typeof handleFetch<Comment>>>
+
+interface CommentAction {
+  payload: CommentPayload
+  type: string
+}
+
+const commentsSlice = createSlice({
+  name: 'comments',
+  initialState,
+  reducers: {
+    resetState: () => initialState,
+    setPartialState: (state, { payload }: { payload: Partial<CommentsState> }) => {
+      return { ...state, ...payload }
+    },
+  },
+  extraReducers: builder => {
+    const isCommentRelatedAction = (action: any): action is CommentAction => {
+      const typedAction: CommentAction = action
+      return (
+        typedAction.type.endsWith('/fulfilled') &&
+        !!typedAction.payload?.data?.id &&
+        !typedAction.payload?.error
+      )
+    }
+
+    builder.addMatcher(isCommentRelatedAction, (state, action) => {
+      if (action.payload.error) return
+
+      // No se si seria necesario algo como esto
+
+      // const updatedPost = action.payload.data
+
+      // Actualizar detalle si aplica
+      // if (state.postDetail?.id === updatedPost.id) {
+      //   state.postDetail = updatedPost
+      // }
+
+      // Actualizar post en lista si existe
+      // const index = state.posts.findIndex(post => post.id === updatedPost.id)
+      // if (index === -1) return
+
+      // state.posts[index] = updatedPost
+    })
+  },
+})
+
+export const commentsReducer = commentsSlice.reducer
